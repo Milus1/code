0e6cfdfb67e5dc846befb71c84200eabfb7f5b2b
JRNJeroen <82874378+JRNJeroen@users.noreply.github.com>
2022-01-12 20:14:43 +0100

    Update PC2_clean.R

--- PC2_clean.R
+++ PC2_clean.R
@@ -1,11 +1,19 @@
 library(stringr)
 
-#PC2 must loaded in as all characters (otherwise BOARD ID fails)
-
+#convert into the right data type
 PC2$ID <- as.numeric(PC2$ID)
 PC2$TotalScoreGuess <- as.numeric(PC2$TotalScoreGuess)
 PC2$TimeTaken <- as.numeric(PC2$TimeTaken)
 
-PC2 <- PC2[PC2$ID>=20211210000000,]
+#remove all data from before we started collecting
+PC2 <- PC2[PC2$ID>=20211210000000,] 
+
+#remove participants with time taken longer than 25 minutes or quicker than 30 seconds
 PC2 <- PC2[PC2$TimeTaken<25 & PC2$TimeTaken > 0.5,]
-PC2 <- cbind(PC2,'GreySquares' = str_count(PC2$board.ID,"1"))
+
+#remove all participants with an NA value
+PC2 <- PC2[PC2$ID != PC2[apply(is.na(PC2),1,sum),1],] 
+
+#calculate the grey squares
+PC2 <- cbind(PC2,'GreySquares' = str_count(PC2$board.ID,"1")) #add column 
+
