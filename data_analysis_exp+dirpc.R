8e85b4d4b4c78a358fabb713b91d93c40f534ef8
Milus1 <79589398+Milus1@users.noreply.github.com>
2022-01-13 16:55:37 +0100

    Update data_analysis_exp+dirpc.R

--- data_analysis_exp+dirpc.R
+++ data_analysis_exp+dirpc.R
@@ -110,26 +110,56 @@ means_p_1
 par(mfrow = c(2, 2))
 
 # p = 0.1
-plot(x = 1:4, y = means_p_0.1[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0.5, 1), main = "p reward = 0.1", lwd = 2)
+plot(x = 1:4, y = means_p_0.1[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0, 1), main = "p reward = 0.1", lwd = 2, xaxt = "n")
+axis(side=1, at=seq(1,4,1))
 lines(x = 1:4, y = means_p_0.1[1, ], col = "green", lwd = 2)
-legend(x = "bottomright", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+legend(x = "bottomleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+lines(x = rep(1:4, each = 2), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[4]]), type = "p", col = "red", lwd = 2)
+
+lines(x = rep(1:4, each = 6), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[4]]), col = "green", lwd = 2, type = "p")
+
 
 
 # p = 0.2
-plot(x = 1:4, y = means_p_0.2[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0.5, 1), main = "p reward = 0.2", lwd = 2)
+plot(x = 1:4, y = means_p_0.2[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0, 1), main = "p reward = 0.2", lwd = 2, xaxt = "n")
+axis(side=1, at=seq(1,4,1))
 lines(x = 1:4, y = means_p_0.2[1, ], col = "green", lwd = 2)
-legend(x = "bottomright", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+legend(x = "bottomleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+lines(x = rep(1:4, each = 6), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[2]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[2]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[2]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[2]), 205:208][[4]]), type = "p", col = "red", lwd = 2)
+
+lines(x = rep(1:4, each = 3), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[1]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[1]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[1]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[1]), 205:208][[4]]), col = "green", lwd = 2, type = "p")
+
+
+
 
 
 # p = 0.4
-plot(x = 1:4, y = means_p_0.4[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0.5, 1), main = "p reward = 0.4", lwd = 2)
+plot(x = 1:4, y = means_p_0.4[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0, 1), main = "p reward = 0.4", lwd = 2, xaxt = "n")
+axis(side=1, at=seq(1,4,1))
 lines(x = 1:4, y = means_p_0.4[1, ], col = "green", lwd = 2)
-legend(x = "bottomright", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+legend(x = "bottomleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+lines(x = rep(1:4, each = 5), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[2]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[2]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[2]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[2]), 205:208][[4]]), type = "p", col = "red", lwd = 2)
+
+lines(x = rep(1:4, each = 8), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[1]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[1]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[1]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[1]), 205:208][[4]]), col = "green", lwd = 2, type = "p")
+
+
 
 # p = 1
-plot(x = 1:4, y = means_p_1[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0.5, 1), main = "p reward = 1", lwd = 2)
+plot(x = 1:4, y = means_p_1[2, ], type = "l", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0, 1), main = "p reward = 1", lwd = 2, xaxt = "n")
+axis(side=1, at=seq(1,4,1))
 lines(x = 1:4, y = means_p_1[1, ], col = "green", lwd = 2)
-legend(x = "bottomright", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+legend(x = "bottomleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+lines(x = rep(1:4, each = 7), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[2]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[2]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[2]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[2]), 205:208][[4]]), type = "p", col = "red", lwd = 2)
+
+lines(x = rep(1:4, each = 8), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[1]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[1]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[1]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[1]), 205:208][[4]]), col = "green", lwd = 2, type = "p")
+
+
+
+
 
 
 ###### DIRECT MEASURE OF PERCEIVED CONTROL ######
@@ -178,6 +208,247 @@ exp_dirpc <- cbind(data_exploration, data_dir_pc[ ,2:5])
 head(data_exploration[, 1]) == head(data_dir_pc[, 1])
 
 
+
+# knowing how many people from each condition participated in the study
+ps <- c(0.1, 0.2, 0.4, 1)
+cond <- c("CTRL", "YOK")
+sample <- data.frame()
+
+for(j in 1:4) {
+  for(i in 1:2) {
+    sample[i, j] <- sum(exp_dirpc[, 2] == ps[j] & exp_dirpc[, 3] == cond[i])
+    
+  }
+}
+colnames(sample) <- c(ps)
+rownames(sample) <- c(cond)
+
+
+
+
+# mean of direct measure of perceived controllability per condition per block of trials
+
+# for the reward frequency condition p = 0.1
+dir_p_0.1 <- data.frame()
+
+for(i in 1:4) { # looping over the block
+  for(j in 1:2) { # looping over control condition (control/yoked)
+    dir_p_0.1[j, i] <- mean(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[j]), 208+i])
+  }
+} 
+
+for(i in 1:2) {
+  for(j in 1:2) {
+    dir_p_0.1[i, j] <- mean(as.numeric(dir_p_0.1[i, (2*j-1):(2*j)]))
+  }
+}
+dir_p_0.1 <- dir_p_0.1[1:2, 1:2]
+colnames(dir_p_0.1) <- c(paste("Half", 1:2))
+rownames(dir_p_0.1) <- c(paste("Control", ps[1]), paste("Yoked", ps[1]))
+
+
+
+# for the reward frequency condition p = 0.2
+dir_p_0.2 <- data.frame()
+
+for(i in 1:4) { # looping over the block
+  for(j in 1:2) { # looping over control condition (control/yoked)
+    dir_p_0.2[j, i] <- mean(exp_dirpc[which(exp_dirpc[, 2] == ps[2] & exp_dirpc[, 3] == cond[j]), 208+i])
+  }
+} 
+
+for(i in 1:2) {
+  for(j in 1:2) {
+    dir_p_0.2[i, j] <- mean(as.numeric(dir_p_0.2[i, (2*j-1):(2*j)]))
+  }
+}
+dir_p_0.2 <- dir_p_0.2[1:2, 1:2]
+colnames(dir_p_0.2) <- c(paste("Half", 1:2))
+rownames(dir_p_0.2) <- c(paste("Control", ps[2]), paste("Yoked", ps[2]))
+
+
+# for the reward frequency condition p = 0.4
+dir_p_0.4 <- data.frame()
+
+for(i in 1:4) { # looping over the block
+  for(j in 1:2) { # looping over control condition (control/yoked)
+    dir_p_0.4[j, i] <- mean(exp_dirpc[which(exp_dirpc[, 2] == ps[3] & exp_dirpc[, 3] == cond[j]), 208+i])
+  }
+} 
+
+for(i in 1:2) {
+  for(j in 1:2) {
+    dir_p_0.4[i, j] <- mean(as.numeric(dir_p_0.4[i, (2*j-1):(2*j)]))
+  }
+}
+dir_p_0.4 <- dir_p_0.4[1:2, 1:2]
+colnames(dir_p_0.4) <- c(paste("Half", 1:2))
+rownames(dir_p_0.4) <- c(paste("Control", ps[3]), paste("Yoked", ps[3]))
+
+
+
+
+# for the reward frequency condition p = 1
+dir_p_1 <- data.frame()
+
+for(i in 1:4) { # looping over the block
+  for(j in 1:2) { # looping over control condition (control/yoked)
+    dir_p_1[j, i] <- mean(exp_dirpc[which(exp_dirpc[, 2] == ps[4] & exp_dirpc[, 3] == cond[j]), 208+i])
+  }
+} 
+
+for(i in 1:2) {
+  for(j in 1:2) {
+    dir_p_1[i, j] <- mean(as.numeric(dir_p_1[i, (2*j-1):(2*j)]))
+  }
+}
+dir_p_1 <- dir_p_1[1:2, 1:2]
+colnames(dir_p_1) <- c(paste("Half", 1:2))
+rownames(dir_p_1) <- c(paste("Control", ps[4]), paste("Yoked", ps[4]))
+
+
+dir_p_0.1
+dir_p_0.2
+dir_p_0.4
+dir_p_1
+
+
+par(mfrow = c(2, 2))
+
+# p = 0.1
+plot(x = 1:2, y = dir_p_0.1[2, ], type = "l", xlab = "Half", ylab = "Explicit PC", col = "red", ylim = c(0, 10), main = "p reward = 0.1", lwd = 1.5, xaxt = "n")
+lines(x = 1:2, y = dir_p_0.1[2, ], col = "red", lwd = 2, type = "p")
+axis(side=1, at=seq(1,2,1))
+lines(x = 1:2, y = dir_p_0.1[1, ], col = "green", lwd = 1.5, type = "l")
+lines(x = 1:2, y = dir_p_0.1[1, ], col = "green", lwd = 2, type = "p")
+legend(x = "topleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+
+# p = 0.2
+plot(x = 1:2, y = dir_p_0.2[2, ], type = "l", xlab = "Half", ylab = "Explicit PC", col = "red", ylim = c(0, 10), main = "p reward = 0.2", lwd = 1.5, xaxt = "n")
+lines(x = 1:2, y = dir_p_0.2[2, ], col = "red", lwd = 2, type = "p")
+axis(side=1, at=seq(1,2,1))
+lines(x = 1:2, y = dir_p_0.2[1, ], col = "green", lwd = 1.5, type = "l")
+lines(x = 1:2, y = dir_p_0.2[1, ], col = "green", lwd = 2, type = "p")
+legend(x = "topleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+
+# p = 0.4
+plot(x = 1:2, y = dir_p_0.4[2, ], type = "l", xlab = "Half", ylab = "Explicit PC", col = "red", ylim = c(0, 10), main = "p reward = 0.4", lwd = 1.5, xaxt = "n")
+lines(x = 1:2, y = dir_p_0.4[2, ], col = "red", lwd = 2, type = "p")
+axis(side=1, at=seq(1,2,1))
+lines(x = 1:2, y = dir_p_0.4[1, ], col = "green", lwd = 1.5, type = "l")
+lines(x = 1:2, y = dir_p_0.4[1, ], col = "green", lwd = 2, type = "p")
+legend(x = "topleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+# p = 1
+plot(x = 1:2, y = dir_p_1[2, ], type = "l", xlab = "Half", ylab = "Explicit PC", col = "red", ylim = c(0, 10), main = "p reward = 1", lwd = 1.5, xaxt = "n")
+lines(x = 1:2, y = dir_p_1[2, ], col = "red", lwd = 2, type = "p")
+axis(side=1, at=seq(1,2,1))
+lines(x = 1:2, y = dir_p_1[1, ], col = "green", lwd = 1.5, type = "l")
+lines(x = 1:2, y = dir_p_1[1, ], col = "green", lwd = 2, type = "p")
+legend(x = "topleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+
+
+
+# visualizing the data
+
+
+plot(x = rep(1:4, each = 2), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208][[4]]), type = "p", xlab = "Block of trials", ylab = "Exploration rate", col = "red", ylim = c(0, 1), main = "p reward = 0.1", lwd = 2, xaxt = "n")
+axis(side=1, at=seq(1,4,1))
+
+lines(x = rep(1:4, each = 6), y = c(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[1]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[2]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[3]], exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[1]), 205:208][[4]]), col = "green", lwd = 2, type = "p")
+
+legend(x = "bottomleft", legend = c("Control", "Yoked"), col = c("green", "red"), pch = 15)
+
+
+
+
+as.numeric(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208])
+
+
+
+as.vector(exp_dirpc[which(exp_dirpc[, 2] == ps[1] & exp_dirpc[, 3] == cond[2]), 205:208])[[1]]
+
+
+
+
+
+install.packages("rstatix")
+library(rstatix)
+
+
+levene_test()
+
+
+
+
+means_p_0.1
+means_p_0.2
+means_p_0.4
+means_p_1
+
+
+final_exp <- exp_dirpc[ , c(1:3 , 205:208)]
+
+
+final_exp <- final_exp %>% gather(key = "time", value = "explore", `Explo Block 1`, `Explo Block 2`, `Explo Block 3`, `Explo Block 4`) %>% convert_as_factor(time)
+
+
+summary <- final_exp %>%
+  group_by(Reward.Frequency, Control.Condition, time) %>%
+  get_summary_stats(explore, type = "mean_sd")
+
+
+final_exp %>%
+  group_by(Reward.Frequency, Control.Condition, time) %>%
+  identify_outliers(explore)
+
+
+
+
+
+final_exp %>%
+  group_by(Reward.Frequency, Control.Condition, time) %>%
+  shapiro_test(explore)
+
+
+ggqqplot(final_exp, "explore", ggtheme = theme_bw()) +
+  facet_grid(explore ~ Reward.Frequency, labeller = "label_both")
+
+
+final_exp %>%
+  group_by(time) %>%
+  levene_test(explore ~ Reward.Frequency*Control.Condition)
+
+
+res.aov <- anova_test(
+  data = final_exp, dv = explore, wid = id,
+  within = time, between = c(Reward.Frequency, Control.Condition)
+)
+get_anova_table(res.aov)
+
+
+
+install.packages("afex")
+library(afex)
+
+mixed_anova <- aov_ez(
+  id = "ID",
+  dv = "explore",
+  data = final_exp,
+  between = "Reward.Frequency",
+  within = "time"
+)
+
+
+
+
+
+# calculate time clicks
+# graphs/visualise
+# confirmatory vs exploratory analyses
 
 # calculate time clicks
 # graphs/visualise
