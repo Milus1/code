21b3c042041a8d0c0635b254e60914f83b173a7c
hoyeonwon <93546861+hoyeonwon@users.noreply.github.com>
2022-01-15 14:32:29 +0100

    Update PC1 data.R

--- PC1 data.R
+++ PC1 data.R
@@ -1,4 +1,4 @@
-#### Perceived controllability ####
+# PERCEIVED CONTROLLABLIITY -----------------------------------------------
 library(plyr)
 pc1path <- "C:/Users/ASUS/Documents/3rd Year/Thesis Writing/perceived_control1" 
 data_pc1 <- ldply(list.files(path=pc1path, full.names=TRUE), read.csv, header=TRUE)
@@ -12,6 +12,7 @@ MQpath <-"C:/Users/ASUS/Documents/3rd Year/Thesis Writing/MQ"
 data_MQ <- ldply(list.files(path=MQpath, full.names=TRUE), read.csv, header=TRUE)
 data_exp<-data_exp[which((data_exp$ID %in% data_MQ$ID)==TRUE),] #extract participants who didn't complete the experiment
 data_pc1<- data_pc1[which((data_pc1$ID %in% data_MQ$ID)==TRUE),] #remove participants
+View(data_MQ)
 
 Conditions<-rep(data_exp$Control.Condition,each=16) #for each unique ID
 Reward_Frequency <- rep(data_exp$Reward.Frequency,each=16)
@@ -37,7 +38,7 @@ for(i in 1:(nrow(data_pc1)/4)){
 }
 
 
-Trials<-rep(1:4,times=45) #Trial numbers
+Trials<-rep(1:4,times=59) #Trial numbers
 
 Vardata <- data.frame(var_btw_reward=varbtw,Control=condit,Reward=prob,Trials)
 #divide by blocks
@@ -49,6 +50,7 @@ Block4<- data.frame(Block4_var=Vardata$var_btw_reward[Vardata$Trials=='4'])
 Vardata2<- data.frame(Block1[-3],Block2,Block3,Block4)
 View(Vardata2)
 
+mean(Vardata2$Block2_var[Vardata2$Control==0 & Vardata2$Reward ==1])
 #means of PC1 by condition & reward frequency & Block
 ps<- c(0.1,0.2,0.4,1)
 
@@ -63,11 +65,11 @@ for(m in 1:2) {
   }
 }
 colnames(means_PC1) <- c(paste("Block", 1:4))
-rownames(means_PC1) <- c(paste("Control", ps), paste("Yoked", ps))
+rownames(means_PC1) <- c(paste("Yoked", ps), paste("Control", ps))
 View(means_PC1)
 
-Half1<-rowMeans(means[,c('Block 1', 'Block 2')], na.rm=TRUE)
-Half2<-rowMeans(means[,c('Block 3','Block 4')],na.rm=TRUE)
+Half1<-rowMeans(means_PC1[,c('Block 1', 'Block 2')], na.rm=TRUE)
+Half2<-rowMeans(means_PC1[,c('Block 3','Block 4')],na.rm=TRUE)
 meanshalf_PC1<-cbind(means_PC1,Half1,Half2)
 #each blocks + half
 View(meanshalf_PC1)
@@ -82,15 +84,28 @@ Half2_0.2 <- c(meanshalf_PC1[2,6],meanshalf_PC1[6,6])
 Half2_0.4 <- c(meanshalf_PC1[3,6],meanshalf_PC1[7,6])
 Half2_1 <- c(meanshalf_PC1[4,6],meanshalf_PC1[8,6])
 
-par(mfrow = c(2,1))
+pdf(file = "/Users/ASUS/Documents/3rd Year/Thesis Writing/PC1_plots.pdf")
+par(mfrow = c(2,2))
 
-#Plot for block 1+2 Perceived Controllabliity
-Half1_PC1plot<- data.frame(Half1_0.1,Half1_0.2,Half1_0.4,Half1_1)
-names(Half1_PC1plot) <- c("p=0.1","p=0.2","p=0.4","p=1")
-barplot(height=as.matrix(Half1_PC1plot),main="Block 1+2",ylab="Average Perceived Controllability",beside=TRUE, col=c("red","blue"))
-legend("topleft",c("With-Control","Yoked"),cex=1.0,bty="n",fill=c("red","blue"))
-#Plot for block 3+4 Perceived Controllabliity
-Half2_PC1plot<- data.frame(Half2_0.1,Half2_0.2,Half2_0.4,Half2_1)
-names(Half2_PC1plot) <- c("p=0.1","p=0.2","p=0.4","p=1")
-barplot(height=as.matrix(Half2_PC1plot),main="Block 3+4",ylab="Average Perceived Controllability",beside=TRUE, col=c("red","blue"))
-legend("topleft",c("With-Control","Yoked"),cex=1.0,bty="n",fill=c("red","blue"))
+#Plot for p(reward)=0.1 Perceived Controllabliity
+Low_PC1plot<- data.frame(Half1_0.1,Half2_0.1)
+names(Low_PC1plot) <- c("Half 1","Half 2")
+barplot(height=as.matrix(Low_PC1plot), ylim=c(0,0.2), main="p(reward) = .1",ylab="Average Perceived Controllability",beside=TRUE, col=c("red","blue"))
+legend("topleft",c("Yoked","With-Control"),cex=1.0,bty="n",fill=c("red","blue"))
+#Plot for p(reward)=0.1 Perceived Controllabliity
+Mod_PC1plot<- data.frame(Half1_0.2,Half2_0.2)
+names(Mod_PC1plot) <- c("Half 1","Half 2")
+barplot(height=as.matrix(Mod_PC1plot), ylim=c(0,0.2),main="p(reward) = .2",ylab="Average Perceived Controllability",beside=TRUE, col=c("red","blue"))
+legend("topleft",c("Yoked","With-Control"),cex=1.0,bty="n",fill=c("red","blue"))
+#Plot for p(reward)=0.4 Perceived Controllabliity
+High_PC1plot<- data.frame(Half1_0.4,Half2_0.4)
+names(High_PC1plot) <- c("Half 1","Half 2")
+barplot(height=as.matrix(High_PC1plot), ylim=c(0,0.2),main="p(reward) = .4",ylab="Average Perceived Controllability",beside=TRUE, col=c("red","blue"))
+legend("topleft",c("Yoked","With-Control"),cex=1.0,bty="n",fill=c("red","blue"))
+#Plot for p(reward)=1 Perceived Controllabliity
+EHigh_PC1plot<- data.frame(Half1_1,Half2_1)
+names(EHigh_PC1plot) <- c("Half 1","Half 2")
+barplot(height=as.matrix(EHigh_PC1plot), ylim=c(0,0.2),main="p(reward) = 1",ylab="Average Perceived Controllability",beside=TRUE, col=c("red","blue"))
+legend("topleft",c("Yoked","With-Control"),cex=1.0,bty="n",fill=c("red","blue"))
+
+dev.off()
